3d293782d365103a0fc918f4579ce6a468736df9
Hill Dan <hill.dan@163.com>
2016-11-09 15:51:02 +0800

    add qos2 support

--- session.go
+++ session.go
@@ -15,6 +15,8 @@ type Session struct {
 	sync.RWMutex
 	//have not been completely acknowledged
 	PubOut map[uint16]packet.PublishPacket //packetId->packet
+	//the record of publish packet received which qos ==2
+	PubIn map[uint16]bool //packetId->bool
 
 	//The Client’s subscriptions.
 	Subscript []packet.TopicFilter
@@ -46,6 +48,24 @@ func (s *Session) ResetPubOut() (old map[uint16]packet.PublishPacket) {
 	s.PubOut = make(map[uint16]packet.PublishPacket)
 	return
 }
+
+func (s *Session) AddPubIn(packetId packet.Integer) {
+	s.Lock()
+	s.PubIn[uint16(packetId)] = true
+	s.Unlock()
+}
+func (s *Session) GetPubIn(packetId packet.Integer) (ok bool) {
+	s.RLock()
+	_, ok = s.PubIn[uint16(packetId)]
+	s.RUnlock()
+	return
+}
+func (s *Session) RemovePubIn(packetId packet.Integer) {
+	s.Lock()
+	delete(s.PubIn, uint16(packetId))
+	s.Unlock()
+}
+
 func (s *Session) SetSubscription(sub []packet.TopicFilter) {
 	s.Lock()
 	s.Subscript = sub
@@ -81,6 +101,7 @@ func (s *Session) Unsubscription(unsub []packet.String) {
 func NewSession() *Session {
 	return &Session{
 		PubOut:    make(map[uint16]packet.PublishPacket),
+		PubIn:     make(map[uint16]bool),
 		Subscript: make([]packet.TopicFilter, 0),
 	}
 }
@@ -91,4 +112,15 @@ func (s *Session) Save(key, clientId string, persister Persister) error {
 		return err
 	}
 	return persister.Save(key, clientId, b)
+}
+func (s *Session) MustInit() {
+	if s.PubOut == nil {
+		s.PubOut = make(map[uint16]packet.PublishPacket)
+	}
+	if s.PubIn == nil {
+		s.PubIn = make(map[uint16]bool)
+	}
+	if s.Subscript == nil {
+		s.Subscript = make([]packet.TopicFilter, 0)
+	}
 }
